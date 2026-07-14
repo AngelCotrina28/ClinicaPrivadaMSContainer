ffae97e453fc63f9a8d7dbf866a4bcf0729d9746
JuanTD <toctito12@gmail.com>
2026-07-14 01:17:08 -0500

    Arreglos en el formato de horario

---
+++ Clinica-Atencion-Medica-Service/src/main/java/com/clinica/atencion/controllers/ControllHora.java
@@ -0,0 +1,1578 @@
+package com.clinica.atencion.controllers;
+
+public class ControllHora {
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+     int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:v
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+
+    v
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    v
+    v
+    v
+
+    v
+    v
+
+
+
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+        int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+    int a:
+
+    int a:
+    
+    int a:
+    
+
+
+
+}
